40093670965998f30fd9f80e3c543b01c58ab7ca
mohana804 <mohana.p@algorims.com>
2025-04-02 15:36:11 +0530

    git commit

---
+++ Terraform/vpc.tf
@@ -0,0 +1,24 @@
+
+resource "aws_vpc" "vpc_Application" {
+  cidr_block           = var.vpc_cidr
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "vpc-three-tier"
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
