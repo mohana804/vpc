b1756cf0211cb95db75a8fbc832f47a9100790d3
mohana804 <mohana.p@algorims.com>
2025-04-02 09:15:42 +0530

    18th commit

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -143,14 +143,14 @@ module "web_asg" {
   instance_type = "t3.micro"  
   security_groups = [module.web_sg.security_group_id]  
 
-  user_data = base64encode(<<-EOF  
-    #!/bin/bash  
-    yum install -y httpd  
-    systemctl start httpd  
-    systemctl enable httpd  
-    echo "<h1>Hello from Web Tier</h1>" > /var/www/html/index.html  
-EOF  
-  )  
+   user_data = base64encode(<<EOF  
+     #!/bin/bash  
+     yum install -y httpd  
+     systemctl start httpd  
+     systemctl enable httpd  
+     echo "<h1>Hello from Web Tier</h1>" > /var/www/html/index.html  
+     EOF
+    )
 
   tag_specifications = [  
     {  
